5239ad38d11972315745f252329089aea8ad3e8e
Donny <contact@donny.tokyo>
2024-08-29 12:23:55 +0900

    file name changes

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -1,10 +1,10 @@
 // app/routes/_marketing+/index.tsx
 import { type MetaFunction } from '@remix-run/node'
-import Creemson from '#app/components/creemson/ellemment'
-import Research from '#app/components/creemson/research'
-import Product from '#app/components/creemson/product'
-import Reports from '#app/components/creemson/reports'
-import Roadmap from '#app/components/creemson/roadmap'
+import Creemson from '#app/components/landing/hero'
+import Research from '#app/components/landing/research'
+import Product from '#app/components/landing/product'
+import Reports from '#app/components/landing/reports'
+import Roadmap from '#app/components/landing/roadmap'
 
 export const meta: MetaFunction = () => [{ title: 'ellemment' }]
 
