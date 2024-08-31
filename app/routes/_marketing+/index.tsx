6ee0248d2609f68611796d076bb9b0c7c8cb6678
Donny <contact@donny.tokyo>
2024-08-31 20:19:31 +0900

    cleaning

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -2,8 +2,7 @@
 import { type MetaFunction } from '@remix-run/node'
 import Hero from '#app/components/landing/hero'
 import Discover from '#app/components/landing/discover'
-import Product from '#app/components/landing/product'
-import Reports from '#app/components/landing/reports'
+
 
 export const meta: MetaFunction = () => [{ title: 'ellemment' }]
 
@@ -12,8 +11,6 @@ export default function Index() {
     <main className="font-poppins grid h-full place-items-center">
       <Hero />
       <Discover />
-      <Product />
-      <Reports /> 
     </main>
   )
 }
